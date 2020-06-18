bf81cc960354af741cd52f4c048323fd4a8039e0
unknown <dolemy_rf@hotmail.com>
2020-06-18 23:50:06 +0200

    userList pages

--- yourplaces_original/src/user/pages/Users.js
+++ yourplaces_original/src/user/pages/Users.js
@@ -1,5 +1,13 @@
 import React from 'react';
-const Users = ()=>{
-return <h1>users works</h1>
+import UserList from '../conponents/UsersList';
+const USERS = ()=>{
+    const Users =[{id:'ul',
+    name:'Lokman',
+    image:'https://ionicframework.com/blog/wp-content/uploads/2019/02/react-beta.png',
+    places:3}]
+return <UserList items={USERS}/>
 }
+
+
+
 export default Users;
