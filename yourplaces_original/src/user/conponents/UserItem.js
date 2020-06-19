14e2937b7dc415f8902ae9bd31ed8f4300680605
unknown <dolemy_rf@hotmail.com>
2020-06-20 00:57:14 +0200

    useritem components

--- yourplaces_original/src/user/conponents/UserItem.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import './UserItem.css';
-const UserItem = props =>{
-
-}
-export default UserItem;
--- yourplaces_original/src/user/pages/Users.js
+++ yourplaces_original/src/user/pages/Users.js
@@ -1,13 +1,19 @@
 import React from 'react';
-import UserList from '../conponents/UsersList';
-const USERS = ()=>{
-    const Users =[{id:'ul',
-    name:'Lokman',
-    image:'https://ionicframework.com/blog/wp-content/uploads/2019/02/react-beta.png',
-    places:3}]
-return <UserList items={USERS}/>
-}
 
+import UsersList from '../components/UsersList';
 
+const Users = () => {
+  const USERS = [
+    {
+      id: 'u1',
+      name: 'Max Schwarz',
+      image:
+        'https://images.pexels.com/photos/839011/pexels-photo-839011.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+      places: 3
+    }
+  ];
+
+  return <UsersList items={USERS} />;
+};
 
 export default Users;
