9818c324e26d9a41ad866a08dee946401cc3d01e
unknown <dolemy_rf@hotmail.com>
2020-06-22 22:36:49 +0200

    user places and using dynamic router 49

--- yourplaces_original/src/App.js
+++ yourplaces_original/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import {BrowserRouter as Router,Route,Redirect,Switch} from 'react-router-dom'
 import Users from './user/pages/Users'
 import NewPlace from './places/pages/NewPlace'
+import UserPlaces from './places/pages/UserPlaces'
 import MainNavigation from './shared/components/Navigation/MainNavigation'
 const App = ()=>{
 
@@ -13,9 +14,13 @@ const App = ()=>{
    <Route path = "/" exact>
      <Users/>
    </Route>
+   <Router path ="/:userId/places" exact>
+     <UserPlaces/>
+   </Router>
    <Route path = "/Places/new" exact>
      <NewPlace/>
    </Route>
+
    <Redirect to="/"/>
    </Switch>
    </main>
